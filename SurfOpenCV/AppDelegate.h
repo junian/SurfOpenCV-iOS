0ecbcdc46c1e3e378d129d91478fb42dc41dbb32
Junian <git@junian.net>
2016-03-06 23:51:03 +0700

    Initial Commit

---
+++ SurfOpenCV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SurfOpenCV
+//
+//  Created by Junian on 3/6/16.
+//  Copyright © 2016 Junian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
